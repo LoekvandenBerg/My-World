4cd461cead62f52552f0e06927b541d71d019c81
Loek <L_V_D_BERG@hotmail.com>
2019-12-18 11:11:40 +0100

    Portfolio commit

---
+++ HugeWorld/Assets/Scripts/ObjectHider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHider : MonoBehaviour
+{
+    private Bounds bounds;
+    private GameObject[] hidableObjects;
+
+    private void Awake()
+    {
+        //get the hideable objects
+        hidableObjects = GameObject.FindGameObjectsWithTag("ChunkObject");
+
+        //get bounds of terrain
+        Terrain terrain = GetComponent<Terrain>();
+
+        Vector3 dimensions = new Vector3(terrain.terrainData.heightmapWidth, 1000, terrain.terrainData.heightmapHeight);
+        Vector3 pos = transform.position + new Vector3(dimensions.x / 2, 0, dimensions.z / 2);
+
+        bounds = new Bounds(pos, dimensions);
+    }
+
+    private void OnEnable()
+    {
+        foreach(GameObject obj in hidableObjects)
+        {
+            if(obj != null && bounds.Contains(obj.transform.position))
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (GameObject obj in hidableObjects)
+        {
+            if (obj != null && bounds.Contains(obj.transform.position))
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+}
